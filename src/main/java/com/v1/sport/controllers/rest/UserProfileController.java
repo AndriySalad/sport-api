e7723756cd9c50ba86bd3aff27f5668efed41d68
Andiy Salad <saldj.andre@gmail.com>
2024-05-26 13:09:27 +0300

    Update user profile controller

--- src/main/java/com/v1/sport/controllers/rest/UserProfileController.java
+++ src/main/java/com/v1/sport/controllers/rest/UserProfileController.java
@@ -3,6 +3,7 @@ package com.v1.sport.controllers.rest;
 import com.v1.sport.data.dto.UserProfileDto;
 import com.v1.sport.services.UserProfileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +14,13 @@ public class UserProfileController {
     private final UserProfileService userProfileService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_TRAINER')")
     public UserProfileDto getMyProfile() {
         return userProfileService.getUserProfile();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_TRAINER')")
     public UserProfileDto getUserProfileById(@PathVariable("id") Long id) {
         return userProfileService.getUserProfileById(id);
     }
